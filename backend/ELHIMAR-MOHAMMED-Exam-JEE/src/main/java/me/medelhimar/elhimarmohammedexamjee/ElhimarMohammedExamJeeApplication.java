e08ee16243b0fe52aabcb1952313173d190c61a8
Mohammed El Himar <mohamed.elhimar04@gmail.com>
2026-05-11 09:00:32 +0100

    initial commit

---
+++ backend/ELHIMAR-MOHAMMED-Exam-JEE/src/main/java/me/medelhimar/elhimarmohammedexamjee/ElhimarMohammedExamJeeApplication.java
@@ -0,0 +1,13 @@
+package me.medelhimar.elhimarmohammedexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElhimarMohammedExamJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElhimarMohammedExamJeeApplication.class, args);
+    }
+
+}
---
+++ backend/ELHIMAR-MOHAMMED-Exam-JEE/src/test/java/me/medelhimar/elhimarmohammedexamjee/ElhimarMohammedExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.medelhimar.elhimarmohammedexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElhimarMohammedExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
